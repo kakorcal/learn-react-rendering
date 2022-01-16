91acc3053b0a271d59008b3ed4e80757a318493c
Kenneth Korcal <kakorcal@gmail.com>
2022-01-16 18:46:33 -0500

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
